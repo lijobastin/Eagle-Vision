fbf43d12b19c70ba0ef1d594667a4d1ab37a6cb5
lijobastin <105723290+lijobastin@users.noreply.github.com>
2023-02-27 18:11:00 +0530

    Update Project_Eagle_Vision.py
    commented boto3

--- src/Project_Eagle_Vision.py
+++ src/Project_Eagle_Vision.py
@@ -39,19 +39,19 @@ def load_index_to_label_dict(
     return index_to_class_label_dict
 
 
-def load_files_from_s3(
-        keys: list,
-        bucket_name: str = 'bird-classification-bucket'
-        ) -> list:
-    """Retrieves files from S3 bucket"""
-    s3 = boto3.client('s3')
-    s3_files = []
-    for key in keys:
-        s3_file_raw = s3.get_object(Bucket=bucket_name, Key=key)
-        s3_file_cleaned = s3_file_raw['Body'].read()
-        s3_file_image = Image.open(BytesIO(s3_file_cleaned))
-        s3_files.append(s3_file_image)
-    return s3_files
+# def load_files_from_s3(
+        # keys: list,
+        # bucket_name: str = 'bird-classification-bucket'
+        # ) -> list:
+    # """Retrieves files from S3 bucket"""
+    # s3 = boto3.client('s3')
+    # s3_files = []
+    # for key in keys:
+        # s3_file_raw = s3.get_object(Bucket=bucket_name, Key=key)
+        # s3_file_cleaned = s3_file_raw['Body'].read()
+        # s3_file_image = Image.open(BytesIO(s3_file_cleaned))
+        # s3_files.append(s3_file_image)
+    # return s3_files
 
 
 @st.cache()
@@ -127,40 +127,40 @@ if __name__ == '__main__':
         img = Image.open(file)
         prediction = predict(img, index_to_class_label_dict, model, k=5)
         top_prediction = prediction[0][0]
-        available_images = all_image_files.get(
-            'train').get(top_prediction.upper())
-        examples_of_species = np.random.choice(available_images, size=3)
-        files_to_get_from_s3 = []
+        #available_images = all_image_files.get(
+        #    'train').get(top_prediction.upper())
+        #examples_of_species = np.random.choice(available_images, size=3)
+        #files_to_get_from_s3 = []
 
-        for im_name in examples_of_species:
-            path = os.path.join('train', top_prediction.upper(), im_name)
-            files_to_get_from_s3.append(path)
-        images_from_s3 = load_files_from_s3(keys=files_to_get_from_s3)
+        #for im_name in examples_of_species:
+        #    path = os.path.join('train', top_prediction.upper(), im_name)
+        #    files_to_get_from_s3.append(path)
+        #images_from_s3 = load_files_from_s3(keys=files_to_get_from_s3)
 
-    else:
-        dataset_type = st.sidebar.selectbox(
-            "Data Portion Type", data_split_names)
-        image_files_subset = dtype_file_structure_mapping[dataset_type]
+    # else:
+        # dataset_type = st.sidebar.selectbox(
+            # "Data Portion Type", data_split_names)
+        # image_files_subset = dtype_file_structure_mapping[dataset_type]
 
-        selected_species = st.sidebar.selectbox("Bird Type", types_of_birds)
-        available_images = load_list_of_images_available(
-            all_image_files, image_files_subset, selected_species.upper())
-        image_name = st.sidebar.selectbox("Image Name", available_images)
-        if image_files_subset == 'consolidated':
-            s3_key_prefix = 'consolidated/consolidated'
-        else:
-            s3_key_prefix = image_files_subset
-        key_path = os.path.join(
-            s3_key_prefix, selected_species.upper(), image_name)
-        files_to_get_from_s3 = [key_path]
-        examples_of_species = np.random.choice(available_images, size=3)
+        # selected_species = st.sidebar.selectbox("Bird Type", types_of_birds)
+        # available_images = load_list_of_images_available(
+            # all_image_files, image_files_subset, selected_species.upper())
+        # image_name = st.sidebar.selectbox("Image Name", available_images)
+        # if image_files_subset == 'consolidated':
+            # s3_key_prefix = 'consolidated/consolidated'
+        # else:
+            # s3_key_prefix = image_files_subset
+        # key_path = os.path.join(
+            # s3_key_prefix, selected_species.upper(), image_name)
+        # files_to_get_from_s3 = [key_path]
+        # examples_of_species = np.random.choice(available_images, size=3)
 
-        for im in examples_of_species:
-            path = os.path.join(s3_key_prefix, selected_species.upper(), im)
-            files_to_get_from_s3.append(path)
-        images_from_s3 = load_files_from_s3(keys=files_to_get_from_s3)
-        img = images_from_s3.pop(0)
-        prediction = predict(img, index_to_class_label_dict, model, 5)
+        # for im in examples_of_species:
+            # path = os.path.join(s3_key_prefix, selected_species.upper(), im)
+            # files_to_get_from_s3.append(path)
+        # images_from_s3 = load_files_from_s3(keys=files_to_get_from_s3)
+        # img = images_from_s3.pop(0)
+        # prediction = predict(img, index_to_class_label_dict, model, 5)
 
     st.title("Here is the image you've selected")
     resized_image = img.resize((336, 336))
@@ -177,7 +177,7 @@ if __name__ == '__main__':
                 0] = f'<a href="{link}" target="_blank">{p[0].title()}</a>'
         df.iloc[idx, 1] = p[1]
     st.write(df.to_html(escape=False), unsafe_allow_html=True)
-    st.title(f"Here are three other images of the {prediction[0][0]}")
+    #st.title(f"Here are three other images of the {prediction[0][0]}")
 
-    st.image(images_from_s3)
+    #st.image(images_from_s3)
     # st.title('How it works:')
